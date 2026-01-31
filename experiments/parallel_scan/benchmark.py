b0942a2299e36f5753bee696a2cf3f20394ccc89
Concode0 <nemonanconcode@gmail.com>
2026-01-31 20:16:36 +0900

    chore: update license header's patent number

--- experiments/parallel_scan/benchmark.py
+++ experiments/parallel_scan/benchmark.py
@@ -1,12 +1,16 @@
-# Trajecto: Real-time 3D Trajectory Reconstruction System (Software)
+# Trajecto: Real-time 3D Trajectory Reconstruction System
 # Copyright 2025-2026 Eunkyum Kim <nemonanconcode@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 #
-# NOTICE: This software implements the "Hybrid ESKF-Stateful TCN" logic 
-# protected under ROK Patent Application No. 10-2025-YYYYYYY.
-# Commercial use requires a separate license from the author.
+# NOTICE: This software is protected under the following ROK Patent Applications:
+# 1. Hybrid ESKF-Stateful TCN Architecture (No. 10-2025-0201093)
+# 2. 3D Ground Truth Generation via Hovering Signal Engineering (No. 10-2025-0201092)
+#
+# Commercial use or redistribution of the core logic requires a separate license.
+# For inquiries, contact: nemonanconcode@gmail.com
 
 """
 Benchmark Suite for Parallel Scan Experiments
